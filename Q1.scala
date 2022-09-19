9bd151ade9d0aeef7a727f4a07005fd48e6c3337
KimuthuKisal <87438609+KimuthuKisal@users.noreply.github.com>
2022-09-19 21:17:19 +0530

    Update Q1.scala

--- Q1.scala
+++ Q1.scala
@@ -6,8 +6,8 @@ object Main{
             return new Point( this.x+point.x, this.y+point.y )
         }
 
-        def move( distance:Int ) : Point = {
-            return this.copy( x=x+distance, y=y+distance )
+        def move( dx:Int, dy:Int ) : Point = {
+            return this.copy( x=x+dx, y=y+dy )
         }
 
         def distance( point:Point ) : Double = {
@@ -28,7 +28,7 @@ object Main{
         println("\nPoint 1 : (" + p1.x +"," + p1.y + ")")
         println("Point 2 : (" + p2.x +"," + p2.y + ")")
         println("\nAddition of (" + p1.x +"," + p1.y + ") and (" + p2.x +"," + p2.y + ") : \n" + "(" + p3.x +"," + p3.y + ")")
-        println("\nMove Point 1 by 4: \n" + "(" + p1.move(4).x +"," + p1.move(4).y + ")")
+        println("\nMove Point 1, x by 4 and y by 3 : \n" + "(" + p1.move(4,3).x +"," + p1.move(4,3).y + ")")
         println("\nDistance between Point 1 and Point 2 : \n" + p1.distance(p2))
         println("\nInverted Point 1 : \n" + "(" + p1.invert().x +"," + p1.invert().y + ")")
     }
